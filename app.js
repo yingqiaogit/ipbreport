eece38782a55183d395f1d53cf88ee8607bea4a8
yingqiaojian <qiaoying@hotmail.com>
2015-09-09 11:12:33 -0400

    initial code

--- app.js
+++ app.js
@@ -4,9 +4,13 @@
 // node.js starter application for Bluemix
 //------------------------------------------------------------------------------
 
+require('dotenv').load();
+
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var https = require('https');
+var JSON = require('JSON');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -17,13 +21,58 @@ var app = express();
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, function() {
+// start server on the specified port and binding host
+app.listen(appEnv.port, appEnv.bind, function () {
 
-	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+  // print a message when the server starts listening
+  logger.info('server starting on ' + appEnv.url);
 });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+
